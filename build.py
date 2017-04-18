d2f14458f039d43d18edfc11dafc41924b9d935c
a_teammate <madoe3@web.de>
2017-04-18 05:22:08 +0200

    let the CI builder build with vs 15

--- build.py
+++ build.py
@@ -2,5 +2,5 @@ from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(username="inexorgame", channel="stable")
-    builder.add_common_builds()
+    builder.add_common_builds(pure_c=False,visual_versions=["10", "12", "14", "15"])
     builder.run()
